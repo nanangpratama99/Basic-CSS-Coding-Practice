ad9a5a26e1a05242b284f4ceadc0b0932d91464b
nanangpratama99 <pratamananang05@gmail.com>
2022-11-16 08:11:03 +0700

    Menambahkan folder Javascript Object & Array

---
+++ Javascript Object & Array/script.js
@@ -0,0 +1,74 @@
+const users = [];
+
+users[0] = {
+    name: "Monica",
+    gender: "Female",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: ["Yellow", "Pink", "White", "Purple"],
+    isHavePet: "Yes",
+    education: [
+        {
+            name: "SD 01",
+            city: "Jakarta",
+            graduate: "2016"
+        },
+
+        {
+            name: "SMP 02",
+            city: "Jakarta",
+            graduate: "2019"
+        },
+        {
+            name: "SMA 03",
+            city: "Tangerang"
+        }
+    ],
+
+    favoriteRestaurant: ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Padang", "Tteok", "Sushi"],
+
+},
+
+    users[1] = {
+        name: "Wendy",
+        gender: "Male",
+        age: 23,
+        email: "wendy@dingdong.com",
+        favoriteColor: ["Blue", "Black", "Grey"],
+        isHavePet: "No",
+        education: [
+            {
+                name: "SD 02",
+                city: "Jakarta",
+                graduate: "2010"
+            },
+
+            {
+                name: "SMP 03",
+                city: "Bogor",
+                graduate: "2013"
+            },
+            {
+                name: "SMA 01",
+                city: "Surabaya",
+                graduate: "2016"
+            },
+            {
+                name: "Universitas Maju",
+                city: "Tangerang",
+            }
+        ],
+
+        favoriteRestaurant: ["Tempura", "Bento", "Susyi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"],
+
+    }
+
+console.log("Nama" + " : " + users[1].name);
+console.log("SMA" + " : " + users[1].education[2].name);
+console.log("Kota" + " : " + users[1].education[2].city);
+console.log("Makanan Favorite" + " : " + users[1].favoriteRestaurant[5]);
+
+
+
+
+
